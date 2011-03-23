7f561b128697a5fc879eb241540041adbe998fad
Saimon Moore <saimon@saimonmoore.net>
2011-03-23 10:41:54 +0100

    Moving teambox recipe to teambox cookbook

--- cookbooks/teambox/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-# teambox cookbook
-# This cookbook includes and sets up a teambox server
-#
-require_recipe "passenger_enterprise"
-require_recipe "mysql"
-
---
+++ cookbooks/teambox/teambox/recipes/default.rb
@@ -0,0 +1,9 @@
+# teambox cookbook
+# This cookbook includes and sets up a teambox server
+#
+include_recipe "git"
+include_recipe "imagemagick"
+require_recipe "passenger_enterprise::nginx"
+require_recipe "mysql"
+memcached_instance "teambox"
+
